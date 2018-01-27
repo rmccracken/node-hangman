cf9925c111fdebf352a9fca6bc8005a67bbf4099
Rob McCracken <rob.mccrackenjr@gmail.com>
2018-01-26 20:34:57 -0500

    progress on word constructor

--- word.js
+++ word.js
@@ -1,7 +1,29 @@
-let letters = require('./letters.js');
+let letters = require('./letter.js');
 
 
-function word(value){
+ Word = (value) => {
+	let _this = this; 
 	this.value = value;
 	this.letters = [];
-	this.guessesMade = '';
+	this.solved =false;
+
+	this.getLetters = () => {
+		for (let i = 0; i < _this.word.length; i++){
+		let newLetter = new Letter(this.word[i]);
+		this.letters.push(newLetter);
+		}
+	};
+	this.letterFound = (letterGuessed) => {
+		let eachspace = 0;
+		this.letters.forEach(function(rightLetter){
+			if (right.letter === letterGuessed){
+				rightLetter.visible === true;
+				eachspace++;
+			}
+		})
+		return eachspace;
+	};
+	this.showLetter = () => {
+		
+	}
+};
